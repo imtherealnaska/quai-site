cb14f645502545510c88705a9ca4581ee631f4ea
Karl Kreder <karl.kreder@gmail.com>
2021-09-22 21:37:26 -0500

    got rid of pull-down menus in the header

--- components/Header.vue
+++ components/Header.vue
@@ -15,52 +15,42 @@
         justify-content: space-around;
       "
     >
-      <v-menu open-on-hover offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            class="header-text"
-            color="white"
-            text
-            rounded
-            v-on="on"
-            v-bind="attrs"
-            nuxt
-            to="/"
-          >
-            Home
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item v-for="(item, index) in home" :key="index" :to="item.to">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-menu open-on-hover offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            class="header-text"
-            color="white"
-            text
-            rounded
-            v-on="on"
-            v-bind="attrs"
-            nuxt
-            to="/design"
-          >
-            Protocol Design
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in design"
-            :key="index"
-            :to="item.to"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <v-btn
+        class="header-text"
+        color="white"
+        text
+        rounded
+        v-on="on"
+        v-bind="attrs"
+        nuxt
+        to="/"
+      >
+        Home
+      </v-btn>
+      <v-btn
+        class="header-text"
+        color="white"
+        text
+        rounded
+        v-on="on"
+        v-bind="attrs"
+        nuxt
+        to="/#features"
+      > 
+        Features
+      </v-btn>
+      <v-btn
+        class="header-text"
+        color="white"
+        text
+        rounded
+        v-on="on"
+        v-bind="attrs"
+        nuxt
+        to="/#roadmap" 
+      > 
+        Roadmap
+      </v-btn>       
       <v-btn
         class="header-text"
         color="white"
@@ -79,26 +69,24 @@
       >
         Developers
       </v-btn>
+      <v-btn
+        class="header-text"
+        color="white"
+        text
+        rounded
+        v-on="on"
+        v-bind="attrs"
+        nuxt
+        to="/faq"
+      >
+        FAQ
+      </v-btn>
     </div>
   </v-app-bar>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    home: [
-      { title: 'Features', to: { path: '/', hash: '#features' } },
-      { title: 'About', to: { path: '/', hash: '#about' } },
-      { title: 'Roadmap', to: { path: '/', hash: '#roadmap' } },
-    ],
-    design: [
-      { title: 'Hermes: Merged Mining', to: { path: '/merged-mining' } },
-      { title: 'Janus: Decentralizd Bridges', to: { path: '/bridges' } },
-      { title: 'Plutus: Tokenomics', to: { path: '/tokenomics' } },
-      { title: 'Ares: Incentives', to: { path: '/incentives' } },
-      { title: 'FAQs', to: { path: '/faq' } },
-    ],
-  }),
   methods: {
     openLink(link) {
       window.open(link, '_blank')
