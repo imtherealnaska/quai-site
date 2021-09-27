bfa1c51d63fd1bb1be782620fe2dabacb8bb81bc
Alan Orwick <alan.kev11@gmail.com>
2021-09-27 16:36:49 -0500

    Updated hostname in sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,9 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/sitemap'],
-
+  sitemap: {
+    hostname: 'https://quai.network',
+  },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
